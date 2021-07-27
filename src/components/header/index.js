bb0952a745f535de94855eba76992d6662bcc326
evelyn masso <coding@outofambit.com>
2021-07-27 12:24:52 -0700

    update nav bar

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,9 +4,10 @@ import style from './style';
 
 const Header = () => (
 	<header class={style.header}>
-		<Link href="/"><h1>Jane Doe</h1></Link>
+		<Link href="/"><h1>evelyn masso</h1></Link>
 		<nav>
-			<Link activeClassName={style.active} href="/blogs">Blogs</Link>
+			<Link activeClassName={style.active} href="https://outofambit.format.com">Portfolio</Link>
+			{/* <Link activeClassName={style.active} href="/blogs">Blogs</Link> */}
 			<Link activeClassName={style.active} href="/contact">Contact me</Link>
 		</nav>
 	</header>
