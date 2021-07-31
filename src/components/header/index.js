4286fbc09d3773c01774bdf2e6cc0be32f0549d5
evelyn masso <coding@outofambit.com>
2021-07-30 17:14:37 -0700

    fix portfolio link

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,7 +6,7 @@ const Header = () => (
 	<header class={style.header}>
 		<Link href="/"><h1>evelyn masso</h1></Link>
 		<nav>
-			<Link activeClassName={style.active} href="https://outofambit.format.com">Portfolio</Link>
+			<a href="https://outofambit.format.com">Portfolio &#128279;</a>
 			{/* <Link activeClassName={style.active} href="/blogs">Blogs</Link> */}
 			<Link activeClassName={style.active} href="/contact">Contact me</Link>
 		</nav>
