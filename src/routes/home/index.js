74fbb64529e063b5f5b994142f73f56f51221019
evelyn masso <coding@outofambit.com>
2021-10-12 21:10:55 -0700

    add more links and email

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -28,6 +28,18 @@ const Home = () => {
 					she likes to talk about mixed identities, queer poetry, and her recent love for running. originally from Ohio, she currently lives on unceded Tongva land (near Los Angeles) and uses she/they pronouns.
 					nowadays you can find most of her activity on <a href="https://github.com/outofambit">github</a> and <a href="https://twitter.com/outofambit">twitter</a>.
 					</p>
+					<p class={style.bio}>
+						You can see some of my past work in <a href="https://outofambit.format.com/">this portfolio</a>.
+						Other social media links:
+						<ul>
+							<li><a href="https://www.linkedin.com/in/evelynmasso/">LinkedIn</a> (sometimes I post fun TikTok videos on here)</li>
+							<li><a href="https://www.instagram.com/outofambit/">Instagram</a> (occasional selfies)</li>
+						</ul>
+					</p>
+					<p class={style.bio}>
+						Want to get in touch? You can email me at [hello @ outofambit dot com].
+					</p>
+
 				</div>
 			</div>
 		</div>
